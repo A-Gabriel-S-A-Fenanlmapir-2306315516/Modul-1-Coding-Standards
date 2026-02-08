ca19ec1d8c32900a3f24876bf055567ac573d751
Gebxby <gabrielfenanilmupengetahuan@gmail.com>
2026-02-08 10:57:24 +0700

    update to run success

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -1,4 +1,11 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
 public class Product {
+    private String productId;
+    private  String proudctName;
+    private int productQuantity;
 }
