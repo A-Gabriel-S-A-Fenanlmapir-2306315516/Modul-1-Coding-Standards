ed3253c015ba4cc0b4eaca49c3f169e277f34a3e
Gebxby <gabrielfenanilmupengetahuan@gmail.com>
2026-02-17 22:06:33 +0700

    build gradle update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ val junitJupiterVersion = "5.9.1"
 
 plugins {
 	java
+	jacoco
 	id("org.springframework.boot") version "3.2.2" // Gunakan versi stabil jika 3.5 belum rilis
 	id("io.spring.dependency-management") version "1.1.4"
 }
@@ -58,4 +59,14 @@ tasks.register<Test>("functionalTest") {
 
 tasks.withType<Test>().configureEach {
 	useJUnitPlatform()
+}
+
+tasks.test{
+	filter{
+		excludeTestsMatching("*FunctionalTest")
+	}
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
 }
